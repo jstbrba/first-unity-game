a8cf9eebe8aba36f0015a1ba9f8318c12b28cf61
jstbrba <justinbarba200@gmail.com>
2025-07-30 23:50:41 +0100

    Add player jump
    Jump height is determined by how long the W key is pressed.

--- My First Game/Assets/Scripts/Player/PlayerMovement.cs
+++ My First Game/Assets/Scripts/Player/PlayerMovement.cs
@@ -3,15 +3,33 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float movementSpeed;
+    [SerializeField] private float jumpPower;
+    [SerializeField] private LayerMask groundLayer;
+
     private Rigidbody2D rb;
+    private BoxCollider2D boxCollider;
+
+    private bool grounded;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
     private void Update()
     {
         // MOVEMENT
         rb.linearVelocity = new Vector2(Input.GetAxis("Horizontal") * movementSpeed, rb.linearVelocity.y);
+
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded())
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y + jumpPower);
+        if (Input.GetKeyUp(KeyCode.W) && rb.linearVelocity.y > 0)
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y / 2);
+    }
+
+    private bool isGrounded()
+    {
+        RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        return hit.collider != null;
     }
 }

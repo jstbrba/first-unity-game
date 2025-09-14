1ec9beba204af517c6eb173bc69bb46a3be23513
jstbrba <justinbarba200@gmail.com>
2025-09-15 00:13:54 +0100

    Create model, view and controller for time

---
+++ My First Game/Assets/Scripts/Game/World/TimeCycle/Meta/TimeModel.cs
@@ -0,0 +1,15 @@
+﻿using Utilities;
+public class TimeModel : BaseModel
+{
+    public Observable<bool> IsDayTime { get { return _isDayTIme; } }
+    public Observable<int> Day { get {  return _day; } }
+
+    private Observable<bool> _isDayTIme = new Observable<bool>();
+    private Observable<int> _day = new Observable<int>();
+
+    public override void Initialise(IContext context)
+    {
+        base.Initialise(context);
+        _day.Value = 1;
+    }
+}
---
+++ My First Game/Assets/Scripts/Game/World/TimeCycle/Meta/TimeView.cs
@@ -0,0 +1,16 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class TimeView : MonoBehaviour, IView
+{
+    [SerializeField] private TextMeshProUGUI _dayNightText;
+    [SerializeField] private TextMeshProUGUI _dayCounterText;
+
+    private IContext _context;
+    public void Initialise(IContext context)
+    {
+        _context = context;
+
+
+    }
+}

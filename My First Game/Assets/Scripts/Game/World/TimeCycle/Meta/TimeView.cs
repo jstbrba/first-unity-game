3b4ebfd441c939b086ef4e0a8a3e265dbc8842a2
jstbrba <justinbarba200@gmail.com>
2025-09-15 00:35:05 +0100

    Add logic to time view

--- My First Game/Assets/Scripts/Game/World/TimeCycle/Meta/TimeView.cs
+++ My First Game/Assets/Scripts/Game/World/TimeCycle/Meta/TimeView.cs
@@ -7,10 +7,34 @@ public class TimeView : MonoBehaviour, IView
     [SerializeField] private TextMeshProUGUI _dayCounterText;
 
     private IContext _context;
+    private TimeModel _model;
+
+    private int _day;
+    private bool _isDayTime;
     public void Initialise(IContext context)
     {
         _context = context;
 
+        _model = _context.ModelLocator.Get<TimeModel>();
 
+        _day = _model.Day.Value;
+        _isDayTime = _model.IsDayTime.Value;
+
+        _model.Day.onValueChanged += Model_Day_OnValueChanged;
+        _model.IsDayTime.onValueChanged += Model_IsDayTime_OnValueChanged;
+    }
+
+    private void Model_Day_OnValueChanged(int previous, int current)
+    {
+        _day = current;
+        _dayCounterText.text = "Day: " + _day;
+    }
+    private void Model_IsDayTime_OnValueChanged(bool previous, bool current)
+    {
+        _isDayTime = current;
+        if (_isDayTime)
+            _dayNightText.text = "Daytime";
+        else
+            _dayNightText.text = "Nighttime";
     }
 }
